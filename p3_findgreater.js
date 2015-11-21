da23033e49fc937ab854cf7bc273d42bf470f406
maxwheel <maxwheel@gmail.com>
2015-11-22 00:33:19 +0800

    p3

---
+++ p3_findgreater.js
@@ -0,0 +1,38 @@
+var mongo = require('mongodb').MongoClient,
+    q = require('q'),
+    _ = require('lodash');
+
+var url = 'mongodb://localhost:27017/learnyoumongo';
+var cond = {age:{$gt: _.parseInt(process.argv[2],10)}};
+var collectionName = 'parrots';
+
+var toArray = function(cursor){
+        var def = q.defer();
+        cursor.toArray(function(err, doc){
+                if (err) return def.reject(err);
+                if (doc) return def.resolve(doc);
+                else return def.reject('no doc');
+                });
+        return def.promise;
+};
+
+q.nfcall(mongo.connect, url)
+    .then(function(db){
+            var cursor = db.collection(collectionName).find(cond);
+            //var toArray = q.nbind(cursor.toArray);
+            toArray(cursor).then(function(doc){
+                console.log(doc);
+                })
+            .catch(function(err){console.error(err)})
+            .finally(function(){db.close();});
+
+
+//            cursor.toArray(function(err, doc){
+//                if (err) return console.log(err);
+//                if (doc) console.log(doc);
+//                db.close();
+//                })
+            })
+.catch(function(err){console.error(err)});
+            
+
