d2c40445da2c2f60c8708c881228023d7e7acea2
maxwheel <maxwheel@gmail.com>
2015-11-22 01:02:24 +0800

    p5

---
+++ p5_insert.js
@@ -0,0 +1,25 @@
+var mongo = require('mongodb').MongoClient,
+    q = require('q'),
+    _ = require('lodash');
+
+var url = 'mongodb://localhost:27017/learnyoumongo';
+var collectionName = 'docs';
+var data = {firstName: process.argv[2], lastName: process.argv[3]};
+//data = {firstName: 'aa', lastName: 'last'};
+console.log("data:" + JSON.stringify(data));
+
+q.nfcall(mongo.connect, url)
+    .then(function(db){
+            console.log("get db");
+            db.collection(collectionName)
+            .insertOne(data, function(err, res){
+                if (err) throw err;
+                console.log(res);
+                db.close();
+                });
+            })
+.catch(function(err){
+        console.error(err);
+        });
+            
+
---
+++ readme.txt
@@ -0,0 +1,1 @@
+p5(insert) bug? No output.
